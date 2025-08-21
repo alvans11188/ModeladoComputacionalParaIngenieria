1a8d1555e59d4c5d5a99063bbba91c3698c66771
Alvans <achicallag@unjbg.edu.pe>
2025-08-21 11:06:13 -0500

    agregando archivo pruebas

---
+++ 1matrices/practica/pruebas/prueba.py
@@ -0,0 +1,17 @@
+print("MATRICES")
+
+n = int(input("Ingrese el tamanio de la matriz"))
+matriz = [n]
+
+input("INGRESE LOS DATOS DE LA MATRIZ")
+
+for i in range(n) :
+    for j in range(n) :
+        matriz[i][j] = int()
+    
+input("La matriz ingresada es:")
+
+for i in range(n) :
+    for j in range(n) :
+        print(matriz[i][j])
+
