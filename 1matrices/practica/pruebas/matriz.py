5ee776fe3c133bf358660dd2a8fd786c5974becf
Alvans <achicallag@unjbg.edu.pe>
2025-08-21 11:05:49 -0500

    agregandoa archivos base

---
+++ 1matrices/practica/pruebas/matriz.py
@@ -0,0 +1,8 @@
+#creando una matriz de 2x2
+matriz=[[1,2],[3,4]]
+
+print("La matriz es:")
+for fila in matriz:
+    for elemento in fila:
+        print(elemento, end=' ')
+    print()  # Nueva línea al final de cada fila
