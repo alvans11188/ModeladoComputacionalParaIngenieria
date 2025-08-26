73937d37180413496eebcbe81e982ad37b619fbe
Alvans <achicallag@unjbg.edu.pe>
2025-08-26 11:08:02 -0500

    agregando variables de entradad

---
+++ 1matrices/practica/pruebas/variables.py
@@ -0,0 +1,22 @@
+#asignacion de variables
+
+edad = 30 #variable tipo int
+print(edad)
+
+nombre= "alex" #variable tipo stirng
+print(nombre)
+precio=19.99 #variable tipo flotante
+print(precio)
+
+a=b=C=10 #asignacion multiple de variables
+
+x,y,z = 1,'gaaa',3.14 #asignacion multiple en diferentes variables
+print(x,y,z)
+#entrada de datos
+nombre=input("Ingrese su nomnbre")
+
+edad=int(input("Ingrese su edad "))
+
+altura= float(input("Ingrese su altura"))
+
+print(nombre,edad,altura)
