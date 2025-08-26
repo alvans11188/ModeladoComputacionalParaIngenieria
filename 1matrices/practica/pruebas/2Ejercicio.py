f9f4933ee68c250b7b9c5f1e442b6bfa3570ee72
Alvans <achicallag@unjbg.edu.pe>
2025-08-26 10:14:06 -0500

    agregando ejercicio 2

---
+++ 1matrices/practica/pruebas/2Ejercicio.py
@@ -0,0 +1,8 @@
+#rellene una matriz con numeros consecutivos
+contador=1
+for i in range(1,7):
+    for j in range(1,7):
+        
+        print(contador,end=' ')
+        contador=contador+1
+    print('')
