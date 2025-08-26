928b9ec9156caad3f219337ee9ff202b7ad0e397
Alvans <achicallag@unjbg.edu.pe>
2025-08-26 09:32:07 -0500

    agregando matrices n x n

---
+++ 1matrices/practica/pruebas/matriz3.py
@@ -0,0 +1,17 @@
+#matriz bidimensional en python
+
+for i in range(1,6):
+    for j in range(1,6):
+        print(f"Elemento [{i}][{j}]")
+
+
+print('\n')
+for i in range(1,7):
+    print(i, end=' ')
+print('')
+# for anidado
+#se manejan 2 variables 
+for i in range(1,7): #valores entre 1 y 6
+    for j in range(1,7):
+        print(0, end=' ')
+    print('') #salto de linea
