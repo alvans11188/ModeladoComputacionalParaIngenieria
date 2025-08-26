820fc0f2a9d87b636ebd9271f2db5c44a9daea36
Alvans <achicallag@unjbg.edu.pe>
2025-08-26 10:19:29 -0500

    agregando ejercicio 4

---
+++ 1matrices/practica/pruebas/4Ejercicio.py
@@ -0,0 +1,9 @@
+#cree una matriz 6 x6 invertida
+
+for i in range(1,7):
+    for j in range(1,7):
+        if (i+j==7):
+            print(1,end=' ')
+        else:
+            print(0,end=' ')
+    print('')
