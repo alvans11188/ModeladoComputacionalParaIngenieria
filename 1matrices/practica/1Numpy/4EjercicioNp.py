8e76f6553146d80b871d46e954ca0bf24de68674
Alvans <achicallag@unjbg.edu.pe>
2025-08-26 14:47:23 -0500

    agregando matriz de 3 dimensiones

---
+++ 1matrices/practica/1Numpy/4EjercicioNp.py
@@ -0,0 +1,14 @@
+#cree una matriz de 3 dimensiones usando numpy
+
+import numpy as np
+
+n=int(input())
+fila=columna=n
+matriz1=np.zeros((n,n,n))
+contador=0
+for i in range(n):
+    for j in range(n):
+        for k in range(n):
+          matriz1[i][j][k]  = contador=contador+1
+
+print(matriz1)
