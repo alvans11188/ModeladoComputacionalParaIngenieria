4394fe98d2187323661f26570dca6d206f5686cf
Alexander <achicallag@unjbg.edu.pe>
2025-09-02 20:18:28 -0500

    agregando suma matrices

---
+++ 1matrices/1GuiaDeLabotatorio/1SumaDeMatrices.py
@@ -0,0 +1,21 @@
+import numpy as np
+
+#EJERCICIO 1
+A1=np.array([[1,2],[3,4]])
+B1=np.array([[5,6],[7,8]])
+print("Ejercicio 1\n", A1+B1,"\n")
+
+#EJERCICIO 2
+A2=np.array([[2,0,1],[1,3,2],[0,4,5]])
+B2=np.array([[1,2,3],[4,0,6],[7,8,9]])
+print("Ejercicio 2\n", A2+B2,"\n")
+
+#EJERCICIO 3
+A3=np.array([[1,-2,3],[4,5,-6]])
+A3=np.array([[7,8,-9],[-1,2,3]])
+print("Ejercicio 3:\n",A3+A3,"\n")
+
+#EJERCICIO 4
+A4=np.array([[2,3],[6,8],[10,12]])
+B4=np.array([[1,1],[1,1],[1,1]])
+print("Ejercicio 4: \n", A4+B4,"\n")
